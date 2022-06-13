efda1f03e756234801a473e689cfaced270c8000
Oluwasegun Ige <40037490+Tshergzeh@users.noreply.github.com>
2022-06-13 22:14:06 +0100

    Implemented the LIVES component

--- hangman.py
+++ hangman.py
@@ -15,23 +15,33 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    while len(word_letters) > 0:
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
         # letter used 
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used these letters:', ' '.join(used_letters))
 
         # what the current word is 
         word_list = [letter if letter in used_letters else '-' for letter in word]
-        print('Current word: ', ' '.join(word_list))
+        print('Current word:', ' '.join(word_list))
 
         user_letter = input("Guess a letter: ").upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+                print('Letter is not in word')
         
         elif user_letter in used_letters:
             print("You have already used that character. Please try again.")
         else:
             print("Invalid character. Please try again.")
+
+    if lives == 0:
+        print('You died! Sorry. The word was', word)
+    else:
+        print("You guessed the word correctly!!!")
 
 hangman()
